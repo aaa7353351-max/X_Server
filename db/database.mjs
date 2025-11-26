1313287b70381933f58c5dc48263fdf3e65e595b
aaa7353351-max <aaa7353351@gmail.com>
2025-11-26 11:32:13 +0900

    mongoose적용 완료

--- db/database.mjs
+++ db/database.mjs
@@ -1,11 +1,11 @@
 import { config } from "../config.mjs";
-import MongoDB from "mongodb";
+import Mongoose from "mongoose";
 
 let db;
 // MONGOMONGO
 export async function connectDB() {
-  return MongoDB.MongoClient.connect(config.db.host).then((client) => {
-    db = client.db("aidetect");
+  return Mongoose.connect(config.db.host, {
+    dbName: "aidetect",
   });
 }
 
@@ -13,6 +13,14 @@ export function getUsers() {
   return db.collection("users");
 }
 
-export function getPosts() {
-  return db.collection("posts");
+// 스키마에 기능을 추가
+//  _id 값을 문자열로 변환한 id라는 가상필드 생성
+//  JSON또는 객체 변환 시(응답보낼 때) virtual 필드도 포함되도록 설정
+export function useVirtualId(schema) {
+  schema.virtual("id").get(function () {
+    return this._id.toString();
+  });
+  schema.set("toJSON", { virtual: true });
+
+  schema.set("toObject", { virtual: true });
 }
